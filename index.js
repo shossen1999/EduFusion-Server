c59ebc643b4461a38423b452caf597b8591ddeda
shossen1999 <shossen1999@gmail.com>
2024-08-26 22:00:15 +0600

    cors modification

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            " https://edu-fusion-ab009.web.app",
+        ]
+    })
+);
 
 app.use(express.json());
 
